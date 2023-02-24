ffc20b3d4880f3770fe063fa0911da3c06df50a0
ifleeyo180 <ifleeyo180@gmail.com>
2023-02-24 08:40:45 +0800

    feat

--- api/index.py
+++ api/index.py
@@ -46,7 +46,8 @@ def home():
 # LINE Bot Webhook 接口
 @app.route("/callback", methods=['POST'])
 def callback():
-    handler = WebhookHandler(line_channel_secret)
+    if request.path != '/callback':
+        abort(400)
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     try:
