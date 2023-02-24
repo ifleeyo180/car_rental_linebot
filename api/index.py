92979f3483c4b19b4c0f457f218737dbe17bdc47
ifleeyo180 <ifleeyo180@gmail.com>
2023-02-25 06:24:50 +0800

    feat

--- api/index.py
+++ api/index.py
@@ -29,7 +29,7 @@ line_bot_channel_access_token = os.getenv('LINE_BOT_CHANNEL_ACCESS_TOKEN')
 line_channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 handler = WebhookHandler(line_channel_secret)
 line_bot_api = LineBotApi(line_bot_channel_access_token)
-creds_base64 = os.environ['GOOGLE_SERVICE_KEY']
+creds_base64 = os.environ['GOOGLE_SHEET_CREDENTIALS']
 creds_json = base64.b64decode(creds_base64).decode('utf-8')
 creds = json.loads(creds_json)
 gc = pygsheets.authorize(service_account_info=creds)
