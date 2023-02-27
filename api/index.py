73818c3c07931c8a89d758728747e951755661bd
ifleeyo180 <ifleeyo180@gmail.com>
2023-02-28 05:33:47 +0800

    feat

--- api/index.py
+++ api/index.py
@@ -29,10 +29,11 @@ line_bot_channel_access_token = os.getenv('LINE_BOT_CHANNEL_ACCESS_TOKEN')
 line_channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 handler = WebhookHandler(line_channel_secret)
 line_bot_api = LineBotApi(line_bot_channel_access_token)
-creds_base64 = os.environ['GOOGLE_SHEET_CREDENTIALS']
+creds_base64 = os.getenv('GOOGLE_SERVICE_KEY')
 creds_json = base64.b64decode(creds_base64).decode('utf-8')
 creds = json.loads(creds_json)
 gc = pygsheets.authorize(service_account_info=creds)
+
 
 
 worksheet_headers = ['車牌', '借用人姓名', '借用日期', '還車人姓名', '還車日期', '借用狀態']
